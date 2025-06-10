df1d0552f97970f6cf1c1e27093a8c7eff8a792f
TimewasterKayla <timewasterkayla@gmail.com>
2025-06-10 15:25:02 -0700

    Version Alpha 1.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css';
+// /pages/_app.tsx
+import '@/styles/globals.css'; // Adjust path if needed
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
