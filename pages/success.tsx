39871dd1cffd206b35a28d45064e69230c110970
TimewasterKayla <timewasterkayla@gmail.com>
2025-06-10 18:19:07 -0700

    Version Alpha 2.9

--- pages/success.tsx
+++ pages/success.tsx
@@ -2,7 +2,7 @@ export default function Success() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-6">
       <h1 className="text-3xl font-bold mb-4">Profile Updated!</h1>
-      <p>You look SOOOOO much CUTER NOW BABY!!</p>
+      <p className="text-center">You look SOOOOO much CUTER NOW BABY!!</p>
     </main>
   );
 }
