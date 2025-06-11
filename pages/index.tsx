ff1737c3cc8210ae64d96c575513ce53e1da8eae
TimewasterKayla <timewasterkayla@gmail.com>
2025-06-10 22:48:48 -0700

    Version Alpha 4.7

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-6">
-      <h1 className="text-3xl font-bold mb-6">Connect Twitter</h1>
+      <h1 className="text-3xl font-bold mb-6">💖Suspicious Button💖</h1>
       <button
   className="bg-pink-500 text-white px-6 py-3 rounded-lg hover:bg-pink-600 transition-colors duration-200 font-semibold"
   onClick={redirectToTwitter}
 >
-  Connect with Twitter
+  🎀Click Meee🎀
 </button>
 
     </main>
