dea953f9c673286ec177e05d4ec7b243735a4079
TimewasterKayla <timewasterkayla@gmail.com>
2025-06-11 20:47:04 -0700

    Version Alpha 8.0

--- pages/page1.tsx
+++ pages/page1.tsx
@@ -1,1 +1,11 @@
-import React, { useEffect, useState } from "react";
+// pages/page1.tsx
+
+import React from "react";
+
+export default function Page1() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-black">
+      <h1 className="text-3xl font-bold">Welcome to Page 1!</h1>
+    </div>
+  );
+}
